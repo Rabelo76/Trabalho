b1980e2e36bda462acf3991760ae359adb760388
Rabelo76 <guilhermerabelo714@gmail.com>
2026-08-28 15:53:42 -0300

    Revisão e leves melhoras

--- scripts/signup.js
+++ scripts/signup.js
@@ -13,3 +13,22 @@ function cadastrar() {
         body: JSON.stringify({nome: nome, sobrenome:sobrenome, cpf:cpf, email:email, senha:senha})
     }).then(resposta => resposta.json())
 }
+
+function mascaraCPF(input) {
+    // Remove tudo o que não for número
+    let valor = input.value.replace(/\D/g, '');
+    
+    // Aplica a formatação por etapas de acordo com o tamanho
+    if (valor.length > 3) {
+        valor = valor.replace(/^(\d{3})(\d)/, '$1.$2');
+    }
+    if (valor.length > 6) {
+        valor = valor.replace(/^(\d{3})\.(\d{3})(\d)/, '$1.$2.$3');
+    }
+    if (valor.length > 9) {
+        valor = valor.replace(/^(\d{3})\.(\d{3})\.(\d{3})(\d)/, '$1.$2.$3-$4');
+    }
+    
+    // Atualiza o valor do campo
+    input.value = valor;
+}
